64e0eb89f388fb781b1ece9731864a7cd8c39b3a
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-24 19:30:32 +0100

    files and basic setup done

---
+++ dist/scripts/personajes.js
@@ -0,0 +1,8 @@
+import Rey from "./Rey";
+import Luchador from "./Luchador";
+import Asesor from "./Escudero";
+import Escudero from "./Asesor";
+
+const personajesGrupo = [Rey, Luchador, Asesor, Escudero];
+
+export default personajesGrupo;
